d777c8206c0020c668c6cbd7c028ada5140c90a9
AlexandrMltsv <61178734+AlexandrMltsv@users.noreply.github.com>
2021-05-21 16:32:47 +0700

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -1,14 +1,12 @@
 ################# API-ключи ###############################
 
-vktokenGroup="f44cdb58f26da7b43b489931c952026e7027cf9a499fb0edc66758edda65cd938b54bcaec2eaa8e4cb5a1"
-teletoken="1778615967:AAFayqQFet_DB6c600_dipj8dgJrH_qhu9Q"
+vktokenGroup=""
+teletoken=""
 
-#"7e4cb74c772fb51d51cf9a30b4e4503a0c914c053665d2cabf2289ee8184be2c5fe0ee6ef83ea16a083b9" #anclaw
-#"f44cdb58f26da7b43b489931c952026e7027cf9a499fb0edc66758edda65cd938b54bcaec2eaa8e4cb5a1" #dailybot
+
 
 ################## id групп ###############################
-IdGroupVK=204593208
-#202960000 #anclaw
-#204593208 #dailybot
+IdGroupVK=
 
-idGroupTelegram=-569647443
+
+idGroupTelegram=
