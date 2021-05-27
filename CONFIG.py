e8495eeed116e83a6251515d76c8393d8fa458e4
AlexandrMltsv <61178734+AlexandrMltsv@users.noreply.github.com>
2021-05-27 09:16:35 +0700

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -1,23 +1,18 @@
 ﻿################# API-ключи ###############################
 
-vktokenGroup="f44cdb58f26da7b43b489931c952026e7027cf9a499fb0edc66758edda65cd938b54bcaec2eaa8e4cb5a1"
+vktokenGroup=""
 
-teletoken="1778615967:AAFayqQFet_DB6c600_dipj8dgJrH_qhu9Q"
+teletoken=""
 
-#"7e4cb74c772fb51d51cf9a30b4e4503a0c914c053665d2cabf2289ee8184be2c5fe0ee6ef83ea16a083b9" #dailybot
-
-#"f44cdb58f26da7b43b489931c952026e7027cf9a499fb0edc66758edda65cd938b54bcaec2eaa8e4cb5a1" #anclaw
 
 ################## id групп ###############################
-IdGroupVK=202960000
+IdGroupVK=
 
-#202960000 #anclaw
-#204593208 #dailybot
 
-idGroupTelegram=-569647443
+
+idGroupTelegram=
 
 ################## Адреса ###############################
 PEER_CRUSH_EVENT = 2000000001
-#2000000001
 ################## Списки и строки ###############################
-users_list_warn = " @keyn_prorok  " #теги оповещения в вк о падении
+users_list_warn = "" #теги оповещения в вк о падении
