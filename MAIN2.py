3b27c7ba229395788b154468e03ea6e881c40f01
AlexandrMltsv <61178734+AlexandrMltsv@users.noreply.github.com>
2021-05-27 09:15:41 +0700

    Add files via upload

--- MAIN2.py
+++ MAIN2.py
@@ -1,4 +1,4 @@
-from threading import Thread
+﻿from threading import Thread
 import Server , CONFIG
 
 thread_vk_bot_resend = Thread(target=Server.vk_bot_resend,args=())
@@ -7,5 +7,5 @@ thread_vk_bot_resend.start()
 thread_vk_bot_resend.join()
 Server.vk.messages.send(random_id=Server.random.randint(0, 999999), message=f"WARNING : " 
                                                                             f"{CONFIG.users_list_warn}"
-                                                                            " Поток-2 уничтожен",
+                                                                            " Поток-RESEND уничтожен",
                                                                     peer_id=CONFIG.PEER_CRUSH_EVENT)
