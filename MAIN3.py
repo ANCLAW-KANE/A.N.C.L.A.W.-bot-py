2f214f5648bd52c5125656f0b9c878c9fcd0455e
AlexandrMltsv <61178734+AlexandrMltsv@users.noreply.github.com>
2021-10-20 02:59:09 +0700

    Delete MAIN3.py

--- MAIN3.py
+++
@@ -1,9 +0,0 @@
-from threading import Thread
-import Server , CONFIG
-thread_vkNode = Thread(target=Server.vkNode,args=())
-thread_vkNode.start()
-thread_vkNode.join()
-Server.vk.messages.send(random_id=Server.random.randint(0, 999999), message=f"WARNING : " 
-                                                                            f"{CONFIG.users_list_warn}"
-                                                                            " Поток-VkNode уничтожен",
-                                                                    peer_id=CONFIG.PEER_CRUSH_EVENT)
