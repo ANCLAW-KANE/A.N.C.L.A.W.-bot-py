7b410478aeeb1fc7bf19a49c0e562f5793c9c126
AlexandrMltsv <61178734+AlexandrMltsv@users.noreply.github.com>
2021-10-20 02:58:59 +0700

    Delete MAIN2.py

--- MAIN2.py
+++
@@ -1,9 +0,0 @@
-﻿from threading import Thread
-import Server , CONFIG
-thread_vk_bot_resend = Thread(target=Server.vk_bot_resend,args=())
-thread_vk_bot_resend.start()
-thread_vk_bot_resend.join()
-Server.vk.messages.send(random_id=Server.random.randint(0, 999999), message=f"WARNING : " 
-                                                                            f"{CONFIG.users_list_warn}"
-                                                                            " Поток-RESEND уничтожен",
-                                                                    peer_id=CONFIG.PEER_CRUSH_EVENT)
