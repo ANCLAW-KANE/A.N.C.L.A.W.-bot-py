f7aec51afa4c9819ca96017b54db46d05e498dc6
A.N.C.L.A.W Corporation <61178734+ANCLAW-KANE@users.noreply.github.com>
2023-09-14 19:57:53 +0700

    fix

---
+++ config_form/CONFIG.py
@@ -0,0 +1,15 @@
+﻿################# API-ключи ###############################
+
+vktokenGroup=""
+vktokenUser= ''
+teletoken=""
+full_permission_user_token = ""
+vktokenService = ""
+
+api_id = 0
+api_hash = ""
+
+################## основное для работы ###############################
+IdGroupVK= 0
+ADMIN_JSON_CONFIG = []
+config_file_json = 'CONFIG.json'
