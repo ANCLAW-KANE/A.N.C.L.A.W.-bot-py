bc6c45a89c03751be164be4abf69aa6b72ab6742
A.N.C.L.A.W Corporation <61178734+ANCLAW-KANE@users.noreply.github.com>
2024-01-05 19:31:42 +0700

    fix

--- MAIN.bat
+++ MAIN.bat
@@ -1,1 +1,12 @@
-.\venvvkbot\.venv\Scripts\python.exe .\MAIN.py
+poetry config virtualenvs.path "C:\\Users\\venvs"
+poetry config virtualenvs.in-project false
+cd .\venvvkbot\
+poetry install
+
+@REM заменить на имя окружения из C:\Users\venvs
+set tgbotvenvname=venvvkbot-LAmQ-QDi-py3.11
+
+set pathvenvtg=C:\Users\venvs\%tgbotvenvname%\Scripts\python.exe
+poetry env use %pathvenvtg%
+cd ..
+%pathvenvtg% MAIN.py
--- MAIN3.bat
+++ MAIN3.bat
@@ -1,1 +1,12 @@
-.\venvtgbot\.venv\Scripts\python.exe .\MAIN3.py
+poetry config virtualenvs.path "C:\\Users\\venvs"
+poetry config virtualenvs.in-project false
+cd .\venvtgbot\
+poetry install
+
+@REM заменить на имя окружения из C:\Users\venvs
+set tgbotvenvname=venvtgbot-VUZEa4Ah-py3.11
+
+set pathvenvtg=C:\Users\venvs\%tgbotvenvname%\Scripts\python.exe
+poetry env use %pathvenvtg%
+cd ..
+%pathvenvtg% MAIN3.py
